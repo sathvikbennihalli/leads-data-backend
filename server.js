aedfe1a44bcf571b3ccddcde9d1fda326c765310
“sathvikbennihalli” <sathwikrb21@gmail.com>
2024-06-06 10:13:16 +0530

    Boiler Plate Code

---
+++ server.js
@@ -0,0 +1,27 @@
+import path from "path";
+import express from "express";
+import root from "./routes/root.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", root);
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
